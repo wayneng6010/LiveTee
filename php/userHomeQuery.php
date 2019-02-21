4723dd3b28084704b678afce3ec4787c57ad7b06
Wayne Ng <wayne.ng6010@gmail.com>
2019-02-22 01:34:08 +0800

    ajax again

---
+++ php/userHomeQuery.php
@@ -0,0 +1,44 @@
+<?php
+// include 'verficationAdmin.php';
+	session_start();
+	require_once 'conn.php';
+	
+
+	$sql1 = "SELECT * FROM item";
+	
+	if(isset($_GET['sort'])) {
+		$sort = $_GET['sort'];
+		if ($sort == "Relevance"){
+			$sql1 = "SELECT * FROM item";
+		}
+		else if ($sort == "Popularity"){
+			$sql1 = "SELECT * FROM item ORDER BY `Item_Price` DESC";
+		}
+		else if ($sort == "Lowest Price"){
+			$sql1 = "SELECT * FROM item ORDER BY `Item_Price` ASC";
+		}
+		else if ($sort == "Highest Price"){
+			$sql1 = "SELECT * FROM item ORDER BY `Item_Price` DESC";
+		}
+		else if ($sort == "Latest Arrival"){
+			$sql1 = "SELECT * FROM item ORDER BY `Item_Price` DESC";
+		}
+
+		// echo "<script>
+		// 	var e = document.getElementById('sortSelect');
+		// 	e.value = 'highPri';
+		// </script>";
+		
+	// header('Location: '.$_SERVER['REQUEST_URI'].'?sort=Relevance');
+
+	}
+
+
+	$result1 = mysqli_query($link,$sql1);
+
+
+	// require_once 'html/userHome.html';
+
+	
+
+	?>
--- userHome.php
+++ userHome.php
@@ -1,23 +1,155 @@
-<?php
-// include 'verficationAdmin.php';
-	session_start();
-	require_once 'conn.php';
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home | LiveTee</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+  	<link rel="icon" href="Asset/Image/icon1.png">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script
+	src="https://code.jquery.com/jquery-3.3.1.min.js"
+	integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+	crossorigin="anonymous"></script>
 
-	$sql1 = "SELECT * FROM item";
-	
-	if(isset($_GET['sort'])) {
-		$sort = $_GET['sort'];
-		if ($sort == "Highest Price"){
-			$sql1 = "SELECT * FROM item ORDER BY `Item_Price` DESC";
+	<script type="text/javascript">
+		function loadlink(){
+			$('.itemOuter').load(document.URL +  ' .itemOuter');
+			<?php include 'php/userHomeQuery.php'; ?>
 		}
-	}
-
-
-	$result1 = mysqli_query($link,$sql1);
-
-
-	// require_once 'html/userHome.html';
-
+		loadlink();
+		setInterval(function(){
+    		loadlink();
+		}, 1000);
+	</script>
 	
+</head>
+<body id="userBody">
 
-	?>
+	<div id="headerUserTopNav">
+		<div id="topNavLeft">
+
+		</div>
+
+		<div id="topNavRight">
+			<a href="#">Login</a>
+			<a href="#">Sign Up</a>
+			<a href="#">Shop</a>
+			<a href="#">Track My Order</a>
+			<a href="#">Notification</a>
+		</div>
+	</div>
+	
+	<div id="headerUser">
+		<div id="flexLeft">
+			<a href="../html/adminHome.html">
+				<p>LIVETEE</p>
+			</a>
+		</div>
+		<div id="flexMiddle">
+			<form action="/action_page.php" method="get">
+				<input type="text" name="searchTxt">
+				<input type="image" src="Asset/Image/search.png" width="20" height="auto" name="searchBtn">
+			</form>
+		</div>
+		<div id="flexRight1">
+			<img src="Asset/Image/Profile.svg" width="30" height="auto">
+			<p><a href="#">Login</a> / <a href="#">Sign Up</a></p>
+		</div>
+		<div id="flexRight2">
+			<a href="#">
+				<div id="cartDiv">
+					<p>
+					<img src="Asset/Image/cart.svg" width="30" height="auto">
+					<p>Cart</p>
+					</p>
+				</div>
+			</a>
+		</div>
+	</div>
+	<div id="userSidebar">
+
+		<div id="genderSidebar">
+			<p>Gender</p>
+			<div id="genderFlex">
+				<div id="gender1"><a href="#">Women</a></div>
+				<div id="gender2"><a href="#">Men</a><br></div>
+			</div>
+		</div>
+		<hr>
+
+		<div id="collSidebar">
+			<p>Collection</p>
+			<a href="#">Sales</a><br>
+			<a href="#">New</a><br>
+		</div>
+		<hr>
+
+		<div id="clothSidebar">
+			<p>Clothing</p>
+			<a href="#">Dresses</a><br>
+			<a href="#">Tops</a><br>
+			<a href="#">T-Shirts</a><br>
+			<a href="#">Pants</a><br>
+			<a href="#">Shorts</a><br>
+			<a href="#">Jeans</a><br>
+		</div>
+		<hr>
+	</div>
+	
+	<div id="sorting">
+		<p>Sort By</p>
+		<select id="sortSelect">
+			<option value="Relevance">Relevance</option>
+			<option value="Popularity">Popularity</option>
+			<option value="Lowest Price">Lowest Price</option>
+			<option value="Highest Price">Highest Price</option>
+			<option value="Latest Arrival">Latest Arrival</option>
+		</select>
+	</div>
+	
+	<div id="clothing">
+		<div class="itemOuter">
+			<?php
+				
+
+			while($row = mysqli_fetch_assoc($result1)){
+			echo '<div class="itemMiddle">
+				<div class="itemInner">
+				<a href="'.$row['Item_ID'].'">
+					<div class="itemInnest">
+						<div class="slideUpOuter">
+							<img class="image" src="data:image/jpeg;base64,'.base64_encode( $row['Item_Image'] ).'" width="100%" alt="">
+							<div class="slideUpInner">
+								<p>VIEW</p>
+							</div>
+						</div>	
+						<p class="desp">'.$row['Item_Name'].'</p>
+						<p class="price">RM '.$row['Item_Price'].'</p>
+						<p class="sizes">'.$row['Item_Size'].'</p>
+						</div>
+				</a>
+				</div>
+			</div>';
+
+			}
+			?>
+			
+		</div>
+			
+	</div>
+	<script type="text/javascript">
+		var e = document.getElementById('sortSelect');
+		e.value = "<?php echo $_GET['sort'];?>";
+		$('#sortSelect').on('change', function() {
+        	var sort = $("#sortSelect option:selected").text();
+        	var new_url = document.URL.substring(0, document.URL.indexOf('?'));
+        	$(location).attr('href', new_url + '?sort=' + sort);
+        	counter++;
+        });
+
+	</script>
+	
+</body>
+</html>
